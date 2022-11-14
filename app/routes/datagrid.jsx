17556fa083de02d02c294f742ab20133e6dc124b
Chris Benjamin <cbenjamin2009@gmail.com>
2022-11-14 09:41:26 -0800

    phase-one revision branch

--- app/routes/datagrid.jsx
+++ app/routes/datagrid.jsx
@@ -1,25 +1,27 @@
 import { DataGrid } from "@mui/x-data-grid";
-import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import sampleData  from "~/data/data"
-
-export async function loader() {
-    return json(sampleData);
-}
 
 export default function Index() {
 
-    let data = useLoaderData();
+  
     //row data contains a unique ID for each row and sample data with first name, last name, and email address
         // map through our loaded data and return the row fields
-    let rows = data.map((user) => {
-        return {
-            id: user.id,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            email: user.email,
-        }
-    })
+    let rows = [
+        { id: 1, firstName: 'Jamaal', lastName: 'Gorczany',  email: 'jg@example.com' },
+        { id: 2, firstName: 'Deon', lastName: 'Jast', email: 'dj@example.com'},
+        { id: 3, firstName: 'Francisca', lastName: 'Lowe', email: 'fl@example.com'},
+        { id: 4, firstName: 'Christine', lastName: 'Hermiston', email: 'ch@example.com'},
+        { id: 5, firstName: 'Gunner', lastName: 'Will', email: 'gw@example.com'},
+        { id: 6, firstName: 'Ereyn', lastName: 'David', email: 'ed@example.com'},
+        { id: 7, firstName: 'Ambrose', lastName: 'Johnston', email: 'aj@example.com'},
+        { id: 8, firstName: 'Telly', lastName: 'Witting', email: 'tw@example.com'},
+        { id: 9, firstName: 'John', lastName: 'Gutman', email: 'jg@example.com'},
+        { id: 10, firstName: 'Spencer', lastName: 'Kemmer', email: 'sk@example.com'},
+        { id: 11, firstName: 'Holly', lastName: 'Parisian', email: 'hp@example.com'},
+        { id: 12, firstName: 'Jonas', lastName: 'Ambrose', email: 'ja@example.com'},
+        { id: 13, firstName: 'Mark', lastName: 'Smith', email: 'ms@example.com'},
+        { id: 14, firstName: 'Carole', lastName: 'Spencer', email: 'cs@example.com'},
+        { id: 15, firstName: 'Rebecca', lastName: 'Nelson', email: 'rn@example.com'},
+    ]
     // column fields must match row fields, you can assign a header for each column and size if desired
     let columns = [
         {field: 'firstName', headerName: 'First Name', flex: 1},
